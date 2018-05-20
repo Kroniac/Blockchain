b489759f886b248f18b6d625256b66cf2d07ca5c
Farid Ansari <farid12ansari7@gmail.com>
2018-05-21 01:47:29 +0530

    added blockchain list check if list is empty then initialize the list with default value

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,13 @@
 blockchain = []
 
 def get_last_transaction_value():
+  if len(blockchain) < 1:
+    return None
   return blockchain[-1]
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_value(transaction_amount, last_transaction):
+  if last_transaction == None:
+    last_transaction = [1]
   blockchain.append([last_transaction, transaction_amount])
 
 def return_transaction_value():
@@ -11,7 +15,7 @@ def return_transaction_value():
   return user_input
 
 def return_user_choice():
-  user_input = float(input('Your choice: '))
+  user_input = input('Your choice: ')
   return user_input
 
 def print_blockchain_elements():
@@ -19,19 +23,19 @@ def print_blockchain_elements():
     print('Outputting Block')
     print(block)
 
-tx_amount = return_transaction_value()
-add_value(tx_amount)
-
 while True:
   print('Please choose')
   print('1: Add a new transaction block')
   print('2: Output the blockchain blocks')
+  print('q: Quit')
   user_choice = return_user_choice()
-  if user_choice == 1:
+  if user_choice == '1':
     tx_amount = return_transaction_value()
     add_value(tx_amount, get_last_transaction_value())
-  elif user_choice == 2:
+  elif user_choice == '2':
     print_blockchain_elements()
+  elif user_choice == 'q':
+    break
   else:
     print('Invalid choice')
 
