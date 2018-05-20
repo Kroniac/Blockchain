f71df2a0a6ef9a3ccc495fd2f33615742732644d
Farid Ansari <farid12ansari7@gmail.com>
2018-05-20 23:23:13 +0530

    added menu to add transaction and to display blockchain blocks value

--- blockchain.py
+++ blockchain.py
@@ -6,20 +6,32 @@ def get_last_transaction_value():
 def add_value(transaction_amount, last_transaction=[1]):
   blockchain.append([last_transaction, transaction_amount])
 
-def get_user_input():
+def return_transaction_value():
   user_input = float(input('Your transaction amount: '))
   return user_input
 
-tx_amount = get_user_input()4455
-add_value(tx_amount)
+def return_user_choice():
+  user_input = float(input('Your choice: '))
+  return user_input
 
-while True:
-  tx_amount = get_user_input()
-  add_value(tx_amount, get_last_transaction_value())
-
+def print_blockchain_elements():
   for block in blockchain:
     print('Outputting Block')
     print(block)
+
+tx_amount = return_transaction_value()
+add_value(tx_amount)
+
+while True:
+  print('Please choose')
+  print('1: Add a new transaction block')
+  print('2: Output the blockchain blocks')
+  user_choice = return_user_choice()
+  if user_choice == 1:
+    tx_amount = return_transaction_value()
+    add_value(tx_amount, get_last_transaction_value())
+  else:
+    print_blockchain_elements()
 
 
 
